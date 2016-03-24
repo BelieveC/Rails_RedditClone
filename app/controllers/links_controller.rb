a918d62d4219823787697c0255ba871a6bc2a0de
Chaitanya Yadav <cayadav2009@gmail.com>
2016-03-24 11:43:52 +0530

    Authorization For edit and destroy

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,6 +1,6 @@
 class LinksController < ApplicationController
   before_action :set_link, only: [:show, :edit, :update, :destroy]
-
+  before_action :authenticate_user!, except: [:index,:show]
   # GET /links
   # GET /links.json
   def index
