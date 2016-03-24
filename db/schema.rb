3adab27071cc294a5b9626a99810db57fca4ac6c
Chaitanya Yadav <cayadav2009@gmail.com>
2016-03-24 18:54:22 +0530

    Fix username bug

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160324073921) do
+ActiveRecord::Schema.define(version: 20160324080042) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "link_id"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 20160324073921) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
